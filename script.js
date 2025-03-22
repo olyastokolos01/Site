65098cf3d7c21cb464229b39329cf23dbb7240ab
Sviatik1803 <svyatoslav182003@gmail.com>
2025-03-22 12:34:03 +0200

    new-new-start

---
+++ script.js
@@ -0,0 +1,30 @@
+const quotes = [
+    {
+        text: "Успіх — це здатність крокувати від однієї невдачі до іншої, не втрачаючи ентузіазму.",
+        author: "Вінстон Черчилль"
+    },
+    {
+        text: "Найкращий спосіб передбачити майбутнє — створити його.",
+        author: "Пітер Друкер"
+    },
+    {
+        text: "Єдиний спосіб робити велику роботу — любити те, що ви робите.",
+        author: "Стів Джобс"
+    },
+    {
+        text: "Життя — це те, що з тобою відбувається, поки ти будуєш інші плани.",
+        author: "Джон Леннон"
+    },
+    {
+        text: "Найкращий час посадити дерево був 20 років тому. Другий найкращий час — сьогодні.",
+        author: "Китайське прислів'я"
+    }
+];
+
+function generateQuote() {
+const randomIndex = Math.floor(Math.random() * quotes.length);
+const quote = quotes[randomIndex];
+document.getElementById('quote').innerHTML = `"${quote.text}" - ${quote.author}`;
+}
+
+window.onload = generateQuote;
